9a3670c8f0d7c2d71950e341e8fc885760a85c23
Jared Oliver <jaredaoliver@gmail.com>
2019-10-26 00:26:47 -0500

    Refactored quantization in test.js, added support for 8-bit and tests were successful

--- test.js
+++ test.js
@@ -11,7 +11,7 @@ if (!fs.existsSync(audioFolder)) {
 
 const sampleRate = 48000;
 const numberOfChannels = 1;
-const bitDepth = 24;
+const bitDepth = 8;
 const signalLength = 1; // length of file in seconds
 
 const bytesPerSample = bitDepth / 8;
@@ -19,15 +19,14 @@ const bytesPerSample = bitDepth / 8;
 const samples = new Uint8Array(sampleRate * bytesPerSample * signalLength);
 
 const sampleLength = 1 / sampleRate; // length of time between samples
-const periodLength = 1 / 45; // length of time of 1 period
+const periodLength = 1 / 440; // length of time of 1 period
 const sineStep = (sampleLength / periodLength) * Math.PI * 2; // value to pass to sine function
 
-const maxUInt = Math.pow(2, bitDepth) - 1;
-const maxInt = (maxUInt + 1) / 2 - 1;
+const quantize = createQuantizer(bitDepth);
 
 for (let i = 0; i < samples.byteLength; i += bytesPerSample) {
-  const realVal = Math.sin((i/bytesPerSample) * sineStep);
-  const quantizedVal = Math.round(realVal * maxInt);
+  const realVal = Math.sin((i/bytesPerSample) * sineStep) * .5;
+  const quantizedVal = quantize(realVal);
   let le = _toLittleEndian(quantizedVal, bytesPerSample);
   for (let ii = 0; ii < le.length; ii += 1) {
     samples[i + ii] = le[ii];
@@ -45,4 +44,14 @@ function _toLittleEndian(decimalValue, byteCount) {
     decimalValue = decimalValue >>> 8;
   }
   return returnArr;
+}
+
+function createQuantizer(bitDepth) {
+  const maxUInt = Math.pow(2, bitDepth) - 1;
+  const maxInt = (maxUInt + 1) / 2 - 1;
+  const maxInt1 = maxInt + 1;
+
+  return bitDepth > 8
+    ? val => Math.round(val * maxInt)
+    : val => Math.round(val * maxInt) + maxInt1;
 }
